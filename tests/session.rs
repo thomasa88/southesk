2d44cd5a637fd4c985bad9f427aa96d169ad3a13
Thomas Axelsson <thomasa88@gmail.com>
2026-05-23 14:28:03 +0200

    tests: Test access token use after 24h in same execution

---
+++ tests/session.rs
@@ -0,0 +1,33 @@
+use std::time::Duration;
+
+use tmr_client::TmrClientBuilder;
+use tokio::time::sleep;
+use tracing::warn;
+
+#[tokio::test]
+#[ignore = "takes very long time"]
+async fn access_token_timeout() -> anyhow::Result<()> {
+    // TODO: Read the actual timeout time from the token and use it.
+
+    tracing_subscriber::fmt().init();
+
+    let montrose = TmrClientBuilder::new("TMR Client Tests")
+        .build()
+        .await?
+        .connect()
+        .await?;
+    warn!("MCP use attempt 1");
+    montrose.get_user_accounts().await?;
+
+    sleep(Duration::from_secs(3600)).await;
+
+    warn!("MCP use attempt 2");
+    montrose.get_user_accounts().await?;
+
+    sleep(Duration::from_secs(25 * 3600)).await;
+
+    warn!("MCP use attempt 3");
+    montrose.get_user_accounts().await?;
+
+    Ok(())
+}
