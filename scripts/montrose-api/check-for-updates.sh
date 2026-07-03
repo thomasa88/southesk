82fe179d58fdd27d359d6b46f84d044479a4249e
Thomas Axelsson <thomasa88@gmail.com>
2026-07-03 19:53:44 +0200

    scripts: Do not rebuild example when running update script

--- scripts/montrose-api/check-for-updates.sh
+++ scripts/montrose-api/check-for-updates.sh
@@ -4,7 +4,8 @@
 
 cd "$(dirname "$0")"
 
-cargo -q r -F __dev --example=devel-introspect > new_api.json
+# Binary built using: cargo b -r -p southesk --example=devel-introspect -F __dev
+../../target/release/examples/devel-introspect > new_api.json
 
 set +e
 DIFF=$(diff -u api.json new_api.json)
